7e899de2508f84001450191dcd2feb096ea7072b
JorgeJoya <jorgelj1995@gmail.com>
2019-06-21 19:20:22 -0600

    BUTTONS: ON/C,. & -/+ EVENTS AND FUNCTIONS

--- js/index.js
+++ js/index.js
@@ -31,7 +31,38 @@ btnFunctions : function(){
 	var multiplicar = document.getElementById("por");
 	var dividir = document.getElementById("dividido");
 	var resultbtn = document.getElementById("igual");
-	var signo = document.getElementById("sign");	
+	var signo = document.getElementById("sign");
+
+onbtn.addEventListener("click",function reset () {
+	onbtnclick = onbtn.innerHTML="0"; 
+	pantalla.innerHTML = onbtnclick; //AL PRESIONAR, SE RESETEA EL VALOR,
+	});
+signo.addEventListener("click", function AlternateSign(){
+	var actual = pantalla.innerHTML.slice(0,8); //HASTA UN MAXIMO DE 8 DIGITOS PREVIOS,		
+	if(actual.includes("-")){
+		var actual = pantalla.innerHTML.slice(0,9)
+		pantalla.innerHTML= actual * -1;	//SI YA HAY UN SIGNO, NO AGREGARLO
+	}else{
+		pantalla.innerHTML = parseFloat(actual) * -1; //CONVIRTIENDO EL NUMERO A NEGATIVO Y VICEVERSA
+	}
+	});
+pnt.addEventListener("click", function punto(){	
+	actual = pantalla.innerHTML.slice(0,7); //HASTA UN MAXIMO DE 8 DIGITOS PREVIOS,
+	pntAdded = pnt.innerHTML=".";
+		if(actual.includes(".")){
+			var actual = pantalla.innerHTML.slice(0,8)
+				pantalla.innerHTML= actual;	//SI YA HAY UN PUNTO, NO AGREGARLO
+		}else if(actual.includes("-")){
+			var actual = pantalla.innerHTML.slice(0,8);
+				pantalla.innerHTML = actual + pntAdded;	//SI YA ESTA ESTA NEGATIVO, SOLO AGREGARLO 	
+		}else if(actual==""){
+			var actual = pantalla.innerHTML.slice(0,8);
+				pantalla.innerHTML = actual + "0" + pntAdded;	//SI YA ESTA ESTA NEGATIVO, SOLO AGREGARLO 	
+		}else{
+			var actual = pantalla.innerHTML.slice(0,7)
+				pantalla.innerHTML = actual + pntAdded; //SI NO HAY UN PUNTO, AGREGARLO
+		}
+	});
 
 
 },
