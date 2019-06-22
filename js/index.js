ec9dcf6f10431666e3af03687d5a73034cfe9bc4
JorgeJoya <jorgelj1995@gmail.com>
2019-06-21 19:59:12 -0600

    NUMBER BUTTONS EVENTS & FUNCTIONS ADDED

--- js/index.js
+++ js/index.js
@@ -63,6 +63,147 @@ pnt.addEventListener("click", function punto(){
 				pantalla.innerHTML = actual + pntAdded; //SI NO HAY UN PUNTO, AGREGARLO
 		}
 	});
+n0.addEventListener("click", function numero0(){
+	actual = pantalla.innerHTML.slice(0,7);	//HASTA UN MAXIMO DE 7 DIGITOS PREVIOS, 
+	n0Added = n0.innerHTML="0";
+		if(pantalla.innerHTML.length == "8"){
+			var actual = pantalla.innerHTML.slice(0,8); //SI YA HAY 8 DIGITOS, NO AGREGAR
+		}else if(actual.includes("-")){
+			var actual = pantalla.innerHTML.slice(0,8);
+				pantalla.innerHTML = actual + n0Added;	//SI YA ESTA ESTA NEGATIVO, SOLO AGREGAR DIGITO 	
+		}else if (n0Added==actual){
+				pantalla.innerHTML = n0Added; //SI EL VALOR ES 0, SOLO SE REEMPLAZA
+		}else{
+				pantalla.innerHTML = actual + n0Added; //SI EL VALOR ES > 0, SE AGREGA EL DIGITO
+		}
+	});
+n1.addEventListener("click", function numero1(){
+	actual = pantalla.innerHTML.slice(0,7);	//HASTA UN MAXIMO DE 7 DIGITOS PREVIOS, 
+	n1Added = n1.innerHTML="1";
+		if(pantalla.innerHTML.length == "8"){
+			var actual = pantalla.innerHTML.slice(0,8); //SI YA HAY 8 DIGITOS, NO AGREGAR
+		}else if(actual.includes("-")){
+			var actual = pantalla.innerHTML.slice(0,8);
+				pantalla.innerHTML = actual + n1Added;	//SI YA ESTA ESTA NEGATIVO, SOLO AGREGAR DIGITO 	
+		}else if (actual!="0"){
+				pantalla.innerHTML = actual + n1Added; //SI EL VALOR ES > 0, SE AGREGA EL DIGITO
+		}else{
+				pantalla.innerHTML = n1Added; //SI EL VALOR ES = 0, SOLO SE REEMPLAZA
+		}	
+	});
+n2.addEventListener("click", function numero2(){	
+	actual = pantalla.innerHTML.slice(0,7);  //HASTA UN MAXIMO DE 7 DIGITOS PREVIOS,
+	n2Added = n2.innerHTML="2";
+	
+		if(pantalla.innerHTML.length == "8"){
+			var actual = pantalla.innerHTML.slice(0,8); //SI YA HAY 8 DIGITOS, NO AGREGAR
+		}else if(actual.includes("-")){
+			var actual = pantalla.innerHTML.slice(0,8);
+				pantalla.innerHTML = actual + n2Added;	//SI YA ESTA ESTA NEGATIVO, SOLO AGREGAR DIGITO 	
+		}else if (actual!="0"){
+				pantalla.innerHTML = actual + n2Added;	//SI EL VALOR ES > 0, SE AGREGA EL DIGITO
+		} else{
+				pantalla.innerHTML = n2Added;	//SI EL VALOR ES = 0, SOLO SE REEMPLAZA
+		}
+	});
+n3.addEventListener("click", function numero3(){	
+	actual = pantalla.innerHTML.slice(0,7);
+	n3Added = n3.innerHTML="3";
+		if(pantalla.innerHTML.length == "8"){
+			var actual = pantalla.innerHTML.slice(0,8); //SI YA HAY 8 DIGITOS, NO AGREGAR
+		}else if(actual.includes("-")){
+			var actual = pantalla.innerHTML.slice(0,8);
+				pantalla.innerHTML = actual + n3Added;	//SI YA ESTA ESTA NEGATIVO, SOLO AGREGAR DIGITO 	
+		}else if (actual!="0"){
+				pantalla.innerHTML = actual + n3Added;	//SI EL VALOR ES > 0, SE AGREGA EL DIGITO
+		}else{
+				pantalla.innerHTML = n3Added;	//SI EL VALOR ES = 0, SOLO SE REEMPLAZA
+		}
+	});
+n4.addEventListener("click", function numero4(){	
+	actual = pantalla.innerHTML.slice(0,7);
+	n4Added = n4.innerHTML="4";
+		if(pantalla.innerHTML.length == "8"){
+			var actual = pantalla.innerHTML.slice(0,8); //SI YA HAY 8 DIGITOS, NO AGREGAR
+		}else if(actual.includes("-")){
+			var actual = pantalla.innerHTML.slice(0,8);
+				pantalla.innerHTML = actual + n4Added;	//SI YA ESTA ESTA NEGATIVO, SOLO AGREGAR DIGITO 	
+		}else if (actual!="0"){
+	   			pantalla.innerHTML = actual + n4Added;	//SI EL VALOR ES > 0, SE AGREGA EL DIGITO
+		}else{
+			 	pantalla.innerHTML = n4Added;	//SI EL VALOR ES = 0, SOLO SE REEMPLAZA
+		} 
+	});
+n5.addEventListener("click", function numero5(){	
+	actual = pantalla.innerHTML.slice(0,7);
+	n5Added = n5.innerHTML="5";
+		if(pantalla.innerHTML.length == "8"){
+			var actual = pantalla.innerHTML.slice(0,8); //SI YA HAY 8 DIGITOS, NO AGREGAR
+		}else if(actual.includes("-")){
+			var actual = pantalla.innerHTML.slice(0,8);
+				pantalla.innerHTML = actual + n5Added;	//SI YA ESTA ESTA NEGATIVO, SOLO AGREGAR DIGITO 	
+		}else if (actual!="0"){
+				pantalla.innerHTML = actual + n5Added;	//SI EL VALOR ES > 0, SE AGREGA EL DIGITO
+		}else{
+				pantalla.innerHTML = n5Added;	//SI EL VALOR ES = 0, SOLO SE REEMPLAZA
+		}
+	});
+n6.addEventListener("click", function numero6(){	
+	actual = pantalla.innerHTML.slice(0,7);
+	n6Added = n6.innerHTML="6";
+		if(pantalla.innerHTML.length == "8"){
+			var actual = pantalla.innerHTML.slice(0,8); //SI YA HAY 8 DIGITOS, NO AGREGAR
+		}else if(actual.includes("-")){
+			var actual = pantalla.innerHTML.slice(0,8);
+				pantalla.innerHTML = actual + n6Added;	//SI YA ESTA ESTA NEGATIVO, SOLO AGREGAR DIGITO 	
+		}else if (actual!="0"){
+				pantalla.innerHTML = actual + n6Added;	//SI EL VALOR ES > 0, SE AGREGA EL DIGITO
+		}else{
+				pantalla.innerHTML = n6Added;	//SI EL VALOR ES = 0, SOLO SE REEMPLAZA
+		}
+	});
+n7.addEventListener("click", function numero7(){	
+	actual = pantalla.innerHTML.slice(0,7);
+	n7Added = n7.innerHTML="7";
+		if(pantalla.innerHTML.length == "8"){
+			var actual = pantalla.innerHTML.slice(0,8); //SI YA HAY 8 DIGITOS, NO AGREGAR
+		}else if(actual.includes("-")){
+			var actual = pantalla.innerHTML.slice(0,8);
+				pantalla.innerHTML = actual + n7Added;	//SI YA ESTA ESTA NEGATIVO, SOLO AGREGAR DIGITO 	
+		}else if (actual!="0"){
+				pantalla.innerHTML = actual + n7Added;	//SI EL VALOR ES > 0, SE AGREGA EL DIGITO
+		}else{
+				pantalla.innerHTML = n7Added;	//SI EL VALOR ES = 0, SOLO SE REEMPLAZA
+		}	
+	});
+n8.addEventListener("click", function numero8(){	
+	actual = pantalla.innerHTML.slice(0,7);
+	n8Added = n8.innerHTML="8";
+		if(pantalla.innerHTML.length == "8"){
+			var actual = pantalla.innerHTML.slice(0,8); //SI YA HAY 8 DIGITOS, NO AGREGAR
+		}else if(actual.includes("-")){
+			var actual = pantalla.innerHTML.slice(0,8);
+				pantalla.innerHTML = actual + n8Added;	//SI YA ESTA ESTA NEGATIVO, SOLO AGREGAR DIGITO 	
+		}else if (actual!="0"){
+				pantalla.innerHTML = actual + n8Added;	//SI EL VALOR ES > 0, SE AGREGA EL DIGITO
+		}else{
+				pantalla.innerHTML = n8Added;
+		}
+	});
+n9.addEventListener("click", function numero9(){	
+	actual = pantalla.innerHTML.slice(0,7);
+	n9Added = n9.innerHTML="9";
+		if(pantalla.innerHTML.length == "8"){
+			var actual = pantalla.innerHTML.slice(0,8); //SI YA HAY 8 DIGITOS, NO AGREGAR
+		}else if(actual.includes("-")){
+			var actual = pantalla.innerHTML.slice(0,8);
+				pantalla.innerHTML = actual + n9Added;	//SI YA ESTA ESTA NEGATIVO, SOLO AGREGAR DIGITO 	
+		}else if (actual!="0"){
+				pantalla.innerHTML = actual + n9Added;	//SI EL VALOR ES > 0, SE AGREGA EL DIGITO
+		}else{
+				pantalla.innerHTML = n9Added;	//SI EL VALOR ES = 0, SOLO SE REEMPLAZA
+		}
+	});
 
 
 },
