d60c32b580828cd4b58d080c09dc6cb8d6e264ef
JorgeJoya <jorgelj1995@gmail.com>
2019-06-21 19:08:30 -0600

    botons effect was added

--- js/index.js
+++ js/index.js
@@ -9,7 +9,20 @@ init : (function(){
 //ASIGNANDO BOTONES Y FUNCIONES
 
 btnFunctions : function(){
+//EFECTO BOTON
+  var btns = document.getElementsByClassName("tecla");
+  for(i=0; i<btns.length;i++){btns[i].setAttribute("onmousedown","ClickDown(this)");
+  	btns[i].setAttribute("onmouseup","ClickUp(this)");	
+  	} 
 
 },
 };
 calculadora.init(); ///INICIALIZANDO LAS FUNCIONES DE LA CALCULADORA
+
+//EFECTO BOTON APLICADOS A LOS ELEMENTOS DEL DOM
+function ClickDown(obj) {
+		obj.style = "padding: 1px; -webkit-transition-duration: 0.1s;-moz-transition-duration: 0.1s; -o-transition-duration: 0.1s;transition-duration: 0.1s;";  
+}
+function ClickUp(obj) {
+ 		obj.style = "padding: 0px;";	 		
+}  
